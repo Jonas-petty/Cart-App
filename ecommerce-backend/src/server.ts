2d81d00777e3d8de00a69d541310d158cc3d7879
Jonas Felix <jonaspetty@hotmail.com>
2025-11-03 23:10:23 +0000

    feat: add the server bootstrap function to validate rabbitmq topology

--- ecommerce-backend/src/server.ts
+++ ecommerce-backend/src/server.ts
@@ -2,14 +2,24 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import { errorHandler } from "./middlewares/error.js";
+import { ensureRabbitTopology } from "./mq/rabbit.js";
 
-const app = express();
+async function bootstrap() {
+    await ensureRabbitTopology();
 
-app.use(cors());
-app.use(express.json());
-app.use(errorHandler);
+    const app = express();
 
-const port = process.env.PORT || 3000;
-app.listen(port, () =>
-    console.log(`[API] listening on http://localhost:${port}`)
-);
+    app.use(cors());
+    app.use(express.json());
+    app.use(errorHandler);
+
+    const port = process.env.PORT || 3000;
+    app.listen(port, () =>
+        console.log(`[API] listening on http://localhost:${port}`)
+    );
+}
+
+bootstrap().catch((error) => {
+    console.error("[API] bootstrap error: ", error);
+    process.exit(1);
+});
