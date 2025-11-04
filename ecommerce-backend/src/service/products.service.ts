7d11d1c79d9e8738853c79152982aa8ef77262eb
Jonas Felix <jonaspetty@hotmail.com>
2025-11-04 19:59:03 +0000

    feat: add the findManyByIds query

--- ecommerce-backend/src/service/products.service.ts
+++ ecommerce-backend/src/service/products.service.ts
@@ -7,6 +7,9 @@ export const ProductService = {
     findById: (id: string) => {
         return prisma.product.findUnique({ where: { id } });
     },
+    findManyByIds: (productIds: Array<string>) => {
+        return prisma.product.findMany({ where: { id: { in: productIds } } });
+    },
     create: (payload: { name: string; price: number; stock: number }) => {
         return prisma.product.create({ data: payload });
     },
