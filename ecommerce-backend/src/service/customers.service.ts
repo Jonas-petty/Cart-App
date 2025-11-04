ddfd5fae6488f6f01fdcd62514a1e6495c067efd
Jonas Felix <jonaspetty@hotmail.com>
2025-11-04 02:42:28 +0000

    feat: update the customer service names

--- ecommerce-backend/src/service/customers.service.ts
+++ ecommerce-backend/src/service/customers.service.ts
@@ -4,10 +4,10 @@ export const CustumerSevice = {
     list: () => {
         prisma.customer.findMany({ orderBy: { createdAt: "desc" } });
     },
-    add: (payload: { name: string; email: string; cpf: string }) => {
+    create: (payload: { name: string; email: string; cpf: string }) => {
         prisma.customer.create({ data: payload });
     },
-    remove: (id: string) => {
+    delete: (id: string) => {
         prisma.customer.deleteMany({ where: { id } });
     },
 };
