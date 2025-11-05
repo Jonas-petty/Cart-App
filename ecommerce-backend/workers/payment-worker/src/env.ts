d25e9836a19963e53f90c25dc70205b9a9c8722c
Jonas Felix <jonaspetty@hotmail.com>
2025-11-05 00:35:10 +0000

    feat: add the env module

---
+++ ecommerce-backend/workers/payment-worker/src/env.ts
@@ -0,0 +1,14 @@
+if (!process.env.RABBITMQ_URL) {
+    throw new Error("RABBITMQ_URL variable is missing on the environment.");
+}
+
+if (!process.env.RABBITMQ_URL) {
+    throw new Error("DATABASE_URL variable is missing on the environment.");
+}
+
+export const env = {
+    ORDER_EXCHANGE: process.env.ORDER_EXCHANGE ?? "order.events",
+    PAYMENT_EXCHANGE: process.env.PAYMENT_EXCHANGE ?? "payment.events",
+    RABBITMQ_URL: process.env.RABBITMQ_URL,
+    DATABASE_URL: process.env.DATABASE_URL,
+};
