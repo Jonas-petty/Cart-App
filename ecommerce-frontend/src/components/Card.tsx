6118af1310d71576ba45092882610856f5ff92f7
Jonas Felix <jonaspetty@hotmail.com>
2025-11-11 15:15:30 +0000

    feat: add the Card component

--- ecommerce-frontend/src/components/Card.tsx
+++ ecommerce-frontend/src/components/Card.tsx
@@ -1,9 +1,9 @@
-function Card() {
-    return (
-        <div>
-            <h2>Card</h2>
-        </div>
-    );
-}
+import styled from "styled-components";
 
-export default Card;
+export const Card = styled.div`
+    background-color: #ffffff;
+    border-radius: 1rem;
+    padding: 1rem;
+    box-shadow: 0 10px 30px rgba(15, 23, 42, 0.06);
+    border: 1px solid #e5e7eb;
+`;
