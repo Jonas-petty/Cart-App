da6ba7a137402c72eb11e50ab1a2bb2d1550a4b8
Jonas Felix <jonaspetty@hotmail.com>
2025-11-11 00:07:32 +0000

    feat: add the global style configuration

--- ecommerce-frontend/src/App.tsx
+++
@@ -1,9 +0,0 @@
-function App() {
-    return (
-        <>
-            <h1>Ecommerce</h1>
-        </>
-    );
-}
-
-export default App;
--- ecommerce-frontend/src/main.tsx
+++ ecommerce-frontend/src/main.tsx
@@ -1,18 +1,52 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./index.css";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
 import Product from "./pages/Product";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body {
+        line-height: 1.5;
+        -webkit-font-smoothing: antialiased;
+    }
+
+    input, button, textarea, select {
+        font: inherit;
+    }
+
+    p, h1, h2, h3, h4, h5, h6 {
+        overflow-wrap: break-word;
+    }
+
+    p {
+        text-wrap: pretty;
+    }
+
+    h1, h2, h3, h4, h5, h6 {
+        text-wrap: balance; 
+    }
+
+    #root {
+        isolation: isolate;
+    }
+`;
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <BrowserRouter>
+            <GlobalStyle />
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/cart" element={<Cart />} />
-                <Route path="/product" element={<Product />} />
+                <Route path="/products" element={<Product />} />
             </Routes>
         </BrowserRouter>
     </StrictMode>
